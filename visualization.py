b93e0667b3873aa05363af5301a45a48a7144ef9
zhangfeiran <feiranzhang@outlook.com>
2019-01-18 21:29:46 +0800

    change visualization

--- visualization.py
+++ visualization.py
@@ -41,28 +41,24 @@ train_df = pd.read_csv('../input/train.csv')
 train_df[f'target_vec'] = train_df['Target'].map(lambda x: list(map(int, x.strip().split())))
 
 
-def make_rgb_image_from_four_channels(channels: list, image_width=512, image_height=512) -> np.ndarray:
-    """
-    It makes literally RGB image from source four channels, 
-    where yellow image will be yellow color, red will be red and so on  
-    """
+def plot_rgby(channels: list, image_width=512, image_height=512) -> np.ndarray:
     rgb_image = np.zeros(shape=(image_height, image_width, 3), dtype=np.float)
     yellow = np.array(Image.open(channels[0]))
-    # yellow is red + green
+
     rgb_image[:, :, 0] += yellow / 2
     rgb_image[:, :, 1] += yellow / 2
-    # loop for R,G and B channels
+
     for index, channel in enumerate(channels[1:]):
         current_image = Image.open(channel)
         rgb_image[:, :, index] += current_image
-    # Normalize image
+
     rgb_image = rgb_image / rgb_image.max() * 255
     return rgb_image.astype(np.uint8)
 
 
 a = 4
 name = [f'../input/train/{train_df["Id"][a]}_{i}.png' for i in colors]
-fig = make_rgb_image_from_four_channels(name)
+fig = plot_rgby(name)
 print([index_class_dict[int(i)] for i in train_df['target_vec'][a]])
 plt.imshow(fig)
 plt.show()
